795170ffe63a88f56025b0014094e9a366d87566
Sidney <sidney_carlini@hotmail.com>
2021-06-11 16:41:16 -0300

    First commit - starting the project

---
+++ Churras/scripts/script.js
@@ -0,0 +1,4 @@
+/*Carne - 400 gr por pessoa + de 6 horas - 650
+Cerveja - 1200ml pessoal + de 6 horas - 2000ml
+Refrigerante/agua - 1000ml pessoal + de 6 horas - 1500ml
+Crianca vale por 0,5.                                 */
