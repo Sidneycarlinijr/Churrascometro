c9b80d86770d7823cf71222ff9c5e78f6bfa8483
Sidney <sidney_carlini@hotmail.com>
2021-06-12 20:02:03 -0300

    trash

---
+++ toDevList/script.js
@@ -0,0 +1,8 @@
+let inputButton = document.getElementById("inputButton");
+
+function addNewTask(){
+    console.log("vc clicou no botao");
+}
+
+
+inputButton.addEventListener("click", addNewTask);  
