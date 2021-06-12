348c72243bce1e3ba45a976b2f2a32447968fc18
Sidney <sidney_carlini@hotmail.com>
2021-06-12 18:48:04 -0300

    coded the input validations, if it is empty a message apears

--- Churras/scripts/script.js
+++ Churras/scripts/script.js
@@ -2,3 +2,23 @@
 Cerveja - 1200ml pessoal + de 6 horas - 2000ml
 Refrigerante/agua - 1000ml pessoal + de 6 horas - 1500ml
 Crianca vale por 0,5.                                 */
+
+let calcButton = document.getElementById("calcButton");
+let kids = document.getElementById("numberOfKids");
+let adults = document.getElementById("numberOfAdults");
+let duration = document.getElementById("duration");
+
+function calc(){
+    let inputKidsNumber = kids.value;
+    let inputAdultsNumber = adults.value;
+    let inputDuration = duration.value;
+
+    if (inputAdultsNumber && inputKidsNumber && inputDuration){
+        console.log("IF")
+    }
+    else{
+        alert("Do not leave any box empty ;)")
+    } 
+}
+
+calcButton.addEventListener("click", calc)
